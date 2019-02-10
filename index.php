fdfe98f9caa7ed33a3e0616a85770628d179e5f0
Rakesh <rakeshpurohit909@gmail.com>
2019-02-10 08:51:39 +0530

    added fb-callback file

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+    $fb = new Facebook\Facebook([
+    'app_id' => '362540437809242', // My facebook app id
+    'app_secret' => '538cd04f971479ff14dc409df2fbcf3b',
+    'default_graph_version' => 'v2.2',
+    ]);
 
-echo "hi";
+  $helper = $fb->getRedirectLoginHelper();
 
-?>
+  $permissions = ['email']; // Optional permissions
+  $loginUrl = $helper->getLoginUrl('fb-callback.php', $permissions);
+
+  echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+  ?>
