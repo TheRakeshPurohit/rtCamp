57ab0f0a0eaf7f0504ea79a719041dfffc648927
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-05-02 13:57:04 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- fb-callback.php
+++ fb-callback.php
@@ -10,37 +10,37 @@
 require_once 'appconfig.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => $appId, // variable with My Facebook App ID
-  'app_secret' => $appSecret,
-  'default_graph_version' => 'v3.2',
-  ]);
+    'app_id' => $appId, // variable with My Facebook App ID
+    'app_secret' => $appSecret,
+    'default_graph_version' => 'v3.2',
+    ]);
 $helper = $fb->getRedirectLoginHelper();
 if(isset($_GET['state'])){
-  $helper->getPersistentDataHandler()->set('state',$_GET['state']);
+    $helper->getPersistentDataHandler()->set('state',$_GET['state']);
 }
 try {
-  $accessToken = $helper->getAccessToken();
+    $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
-  // When Graph returns an error
-  echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
-  // When validation fails or other local issues
-  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
 }
 if (!isset($accessToken)) {
-  if ($helper->getError()) {
+    if ($helper->getError()) {
     header('HTTP/1.0 401 Unauthorized');
     echo "Error: " . $helper->getError() . "\n";
     echo "Error Code: " . $helper->getErrorCode() . "\n";
     echo "Error Reason: " . $helper->getErrorReason() . "\n";
     echo "Error Description: " . $helper->getErrorDescription() . "\n";
-  } else {
+    } else {
     header('HTTP/1.0 400 Bad Request');
     echo 'Bad request hihihih';
-  }
-  exit;
+    }
+    exit;
 }
 // Logged in
 echo '<h3>Access Token</h3>';
@@ -57,18 +57,18 @@ $tokenMetadata->validateAppId($appId); // My Facebook App ID
 //$tokenMetadata->validateUserId('123');
 $tokenMetadata->validateExpiration();
 if (! $accessToken->isLongLived()) {
-  // Exchanges a short-lived access token for a long-lived one
-  try {
+    // Exchanges a short-lived access token for a long-lived one
+    try {
     $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-  } catch (Facebook\Exceptions\FacebookSDKException $e) {
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
     echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
     exit;
-  }
-  echo '<h3>Long-lived</h3>';
-  var_dump($accessToken->getValue());
+    }
+    echo '<h3>Long-lived</h3>';
+    var_dump($accessToken->getValue());
 }
 
-$_SESSION['fb_access_token'] = (string) $accessToken;
+$_SESSION['fb_access_token'] = (string)$accessToken;
 
 //echo $_SESSION['fb_access_token'];
 
--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-  require_once 'appconfig.php';
+    require_once 'appconfig.php';
 
-  $fb = new Facebook\Facebook([
+    $fb = new Facebook\Facebook([
     'app_id' => $appId,
     'app_secret' => $appSecret,
     'default_graph_version' => 'v3.2',
     ]);
   
-  $helper = $fb->getRedirectLoginHelper();
+    $helper = $fb->getRedirectLoginHelper();
   
-  $permissions = ['email','user_photos','user_videos','user_posts','user_link','user_status','user_link']; // Optional permissions
-  $loginUrl = $helper->getLoginUrl($CallbackUrl,$permissions);
+    $permissions = ['email','user_photos','user_videos','user_posts','user_link','user_status','user_link']; // Optional permissions
+    $loginUrl = $helper->getLoginUrl($CallbackUrl,$permissions);
   
-  echo "<h3> Connect Clicking Below Facebook Icon Using Your Facebook Account.<h3>";
-  echo '<a href="' .htmlspecialchars($loginUrl).'"><img alt="Login With Facebook" src="images/fb_icon.png"></a>';
+    echo "<h3> Connect Clicking Below Facebook Icon Using Your Facebook Account.<h3>";
+    echo '<a href="' .htmlspecialchars($loginUrl).'"><img alt="Login With Facebook" src="images/fb_icon.png"></a>';
